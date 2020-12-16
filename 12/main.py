63558486634161fffa055933b9760226e09a3a57
seaerchin <jer005@e.ntu.edu.sg>
2020-12-16 15:46:41 +0800

    day 12 part 2 done

--- 12/main.py
+++ 12/main.py
@@ -21,10 +21,32 @@ def navigate(inst):
             facing = CMD[cmd](num, facing)
         else:
             x, y = CMD[cmd](x, y, num, facing)
-        print(x, y, facing)
     return abs(x) + abs(y)
+
+def waypoint(inst): 
+    ship = complex(0, 0)
+    waypoint = complex(10, 1) 
+
+    CMD = {
+        "N": lambda waypoint, units: waypoint + complex(0, units),
+        "S": lambda waypoint, units: waypoint + complex(0, -units),
+        "E": lambda waypoint, units: waypoint + complex(units, 0),
+        "W": lambda waypoint, units: waypoint + complex(-units, 0),
+        "L": lambda waypoint, units: waypoint * complex(0, 1) ** (units // 90),
+        "R": lambda waypoint, units: waypoint * complex(0, -1) ** (units // 90),
+        "F": lambda ship, waypoint, units: ship + waypoint * units 
+    }
+
+    for i in inst:
+        cmd, units = i[0], int(i[1:])
+        if cmd in "NSEWLR":
+            waypoint = CMD[cmd](waypoint, units)
+        else: 
+            ship = CMD[cmd](ship, waypoint, units)
+    return ship
 
 if __name__ == "__main__":
     with open("data.txt", "r") as f:
         input = f.read().splitlines()
-        print(navigate(input))
+        # print(navigate(input))
+        print(waypoint(input))
